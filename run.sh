2a16e2883b1154795e1c72da4061bbcab3321234
Daniel Botero Correa <daniel_boteroc4@hotmail.com>
2023-03-16 20:42:09 +0100

    ignoring errors

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ export KESTREL_CERTIFICATE_PASSWORD="12345"
 export KESTREL_CERTIFICATE_PATH=/https/aspnetapp.pfx
 export KESTREL_CERTIFICATE_FOLDER=$HOME/.aspnet/http
 
-make create-certificate
+make setup-services --ignore-errors
+make create-certificate --ignore-errors
 
 docker-compose --file "docker-compose.yml" build --force-rm --quiet
 docker-compose --file "docker-compose.yml" up --force-recreate --detach
